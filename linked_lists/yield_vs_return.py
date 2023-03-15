b895c69b084300b80a6fe2dda50c308948c53e72
Filippo <filippocucina5@gmail.com>
2023-03-14 21:33:32 -0400

    New File. Learning about Yield Instruction

---
+++ linked_lists/yield_vs_return.py
@@ -0,0 +1,21 @@
+def cadena():
+    return "Usando la Instruccion Return"
+    print("Imprimiendo con la instruccion Return")
+
+def cadena2():
+    yield "Usando la instruccicon Yield"
+    print("Imprimiendo en Pantalla")
+
+def cadena3(text_return):
+    return
+
+
+def main():
+    print(cadena())
+
+    #TODO
+    #write in the main function: print(cadena2()), print(cadena3())
+    #Keep reading about Yield. Once you finish go back to Visual Studio
+
+if __name__ == "__main__":
+    main()
